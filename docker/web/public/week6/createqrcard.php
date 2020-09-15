df3836e430028ebef47cc58bf97bf0ae28997fbd
Brian Clincy <bclincy@gmail.com>
2020-09-15 19:30:30 -0400

    Made changes to the create qr code file

--- docker/web/public/week6/createqrcard.php
+++ docker/web/public/week6/createqrcard.php
@@ -3,7 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Create Custom Contact with QRCode</title>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 <body>
     <div class="container">
@@ -25,6 +28,13 @@
                             <label class="col-md-4 control-label">Job Title</label>
                             <div class="col-md-8 inputGroupContainer">
                                <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-home"></i></span><input id="jobstitle" name="jobtitle" placeholder="Computer Engineer" class="form-control" required="true" value="" type="text"></div>
+                            </div>
+                         </div>
+                      </fieldset>
+                         <div class="form-group">
+                            <label class="col-md-4 control-label">Address</label>
+                            <div class="col-md-8 inputGroupContainer">
+                               <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-home"></i></span><input id="address" name="address" placeholder="123 Main Street" class="form-control" required="true" value="" type="text"></div>
                             </div>
                          </div>
                       </fieldset>
