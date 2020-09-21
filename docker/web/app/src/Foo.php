0156a7dd845f3a2e2b1633e555df5f17394ef0b6
Brian Clincy <bclincy@gmail.com>
2020-09-21 01:04:50 -0400

    Update Foo to format phone number (#12)

--- docker/web/app/src/Foo.php
+++ docker/web/app/src/Foo.php
@@ -28,4 +28,26 @@ class Foo
         
         return file_exists($saveTo); 
     }
+
+    public function format_phone_us($phone) {
+        // note: making sure we have something
+        if(!isset($phone{3})) { return ''; }
+        // note: strip out everything but numbers 
+        $phone = preg_replace('/[^0-9]/', '', $phone);
+        $length = strlen($phone);
+        switch($length) {
+        case 7:
+            return preg_replace('/([0-9]{3})([0-9]{4})/', '$1-$2', $phone);
+        break;
+        case 10:
+        return preg_replace('/([0-9]{3})([0-9]{3})([0-9]{4})/', '($1) $2-$3', $phone);
+        break;
+        case 11:
+        return preg_replace('/([0-9]{1})([0-9]{3})([0-9]{3})([0-9]{4})/', '$1($2) $3-$4', $phone);
+        break;
+        default:
+            return $phone;
+        break;
+        }
+    }
 }
