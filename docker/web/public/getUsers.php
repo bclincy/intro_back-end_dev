d6f41c2707e6b2a54d150833b47cc1f5204e1501
Louis Maxwell <louismaxwell@knights.ucf.edu>
2020-09-25 03:38:51 -0400

    made more css style changes, reached limit of request

--- docker/web/public/getUsers.php
+++ docker/web/public/getUsers.php
@@ -27,54 +27,57 @@ $info = json_decode($firstUserData, true);
     <title>Document</title>
     <link rel="stylesheet" href="standard.css" />
     <style>
-        .hero img {
+        /* .hero img {
             float: right;
-        }
+
+        } */
 
         /* .hero article {
             /* background-color: white; */
             /* opacity: 0.5; */
              */
 
-        .card-items {
-            display: inline-block;
-        }
+            .card {
+                box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+                max-width: 300px;
+                margin: auto;
+                text-align: center;
+                font-family: arial;
+                background-color: gray;
+            }
         .container {
             display: flex;
-            width: 100%;
+            width: 33%;
         }
+        
     </style>
 </head>
 
 <body>
-    <div class="hero">
         <h1>Display Users</h1>
-        <article>
-
-            <?php
-
-            $i = 0;
-            while ($i <= 4) {
-
-                var_dump($data[$i]);
-                $userdetails = $data[$i];
-                $userData = file_get_contents($userdetails['url'], false, $context);
-                $info = json_decode($userData, true);
-
-            ?>
-                <article>
-                    <h3><?php echo $info["id"]?> User</h3>:
-                    <img src="<?php echo $info['avatar_url']; ?>" alt="User Image"/>
-                    <ul>
-                        <li>Name:<?php echo $info['name']; ?></li>
-                        <li><?php echo $info['name']; ?></li>
-                    </ul>
-                </article>
-            <?php
-                $i++;
-            } ?>
-
-    </div>
+        <div class="container">
+                <?php
+                $i = 0;
+                while ($i <= 4) {
+                    // var_dump($data[$i]);
+                    $userdetails = $data[$i];
+                    $userData = file_get_contents($userdetails['url'], false, $context);
+                    $info = json_decode($userData, true);
+                ?>
+                    <div class="card">
+                        <h3><?php echo $info["id"]?>User</h3>
+                        <img src="<?php echo $info['avatar_url']; ?>" alt="User Image" style="width: 200px" style="height: 200px"/>
+                        <div>
+                            <ul>
+                                <li>Name:<?php echo $info['name']; ?></li>
+                                <li><?php echo $info['name']; ?></li>
+                            </ul>
+                        </div>
+                    </div>
+                <?php
+                    $i++;
+                } ?>
+        </div>
     <p>This Assignment you are getting data from the github's API allowing you to get user
         data. This hits two endpoints user, then get user details. Go through 5 of these results and display
         the data. You can decide how or what data you'd want to display.</p>
